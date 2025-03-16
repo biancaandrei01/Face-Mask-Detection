a892875729eaec741d3ade356a75cdc48cfc6c1a
bianca.andrei <bianca.andrei@realworld-systems.com>
2025-03-16 14:51:27 +0200

    test modificare train

--- code/train.py
+++ code/train.py
@@ -5,8 +5,9 @@ from ultralytics import YOLO
 if __name__ == "__main__":
     # Initialize your Weights and Biases project
     wandb.init(
-        project="???",  # Project name in wandb
-        name="???",  # Name of the run
+        entity="transformers_3",
+        project="Face Mask Detection",  # Project name in wandb
+        name="test",  # Name of the run
         config={
             "epochs": 200,
             "batch_size": 10,
@@ -16,21 +17,20 @@ if __name__ == "__main__":
 
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     # Load the pre-trained model
-    model = YOLO("checkpoints\\???\\weights\\last.pt")  # "C:\\Users\\Bianca\\PycharmProjects\\PoCVodafone\\checkpoints\\full_1\\weights\\last.pt"
-    # "yolov8n.pt"
+    model = YOLO("yolov8n.pt")
     model.to(device)
 
     # Start training
     results = model.train(
         # Path to the dataset.yaml file
-        data='???\\data.yaml',
+        data='C:\\Users\\Bianca\\PycharmProjects\\ACABI\\Face_Mask_Detection\\mask-dataset\\data.yaml',
         epochs=100,  # Number of epochs
         batch=10,  # Batch size
         save=True,  # Save checkpoints after each epoch
         device=device,
         # Directory where the model checkpoints will be saved
         project="checkpoints",
-        name="???",  # Name of the training run folder
+        name="test",  # Name of the training run folder
         optimizer='Adam',  # Optional: you can specify optimizer
         workers=2,  # Number of data loading workers
         verbose=True,  # Display training progress
