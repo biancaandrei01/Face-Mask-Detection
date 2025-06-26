72c914277fdc4f320325dfd88c1f667a21b21be0
bianca.andrei <bianca.andrei@realworld-systems.com>
2025-06-26 12:33:39 +0300

    code refactoring; added gitignore

--- code/test.py
+++ code/test.py
@@ -1,5 +1,13 @@
 from ultralytics import YOLO
+import os.path
+from utils import load_config
+
+def test():
+    config = load_config.load_config()
+
+    model = YOLO(os.path.join(config["checkpoints"], config["wandb"]["name"], "weights", "best.pt"))
+    results = model.val(data=os.path.join(config["dataset"], "data.yaml"),
+                        split='val')
 
 if __name__ == "__main__":
-    model = YOLO("C:\\Users\\Bianca\\PycharmProjects\\ACABI\\Face_Mask_Detection\\checkpoints\\test\\weights\\best.pt")
-    results = model.val(data='C:\\Users\\Bianca\\PycharmProjects\\ACABI\\Face_Mask_Detection\\mask-dataset\\data.yaml', split='val')
+    test()
